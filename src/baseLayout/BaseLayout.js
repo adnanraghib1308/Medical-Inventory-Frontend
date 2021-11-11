2f0d27d5bc5e8b730d9188a4c9c682fcd66102bd
adnanraghib1308 <adnan.raghib@gmail.com>
2021-11-11 15:08:56 +0530

    initial draft

--- src/baseLayout/BaseLayout.js
+++ src/baseLayout/BaseLayout.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import "antd/dist/antd.css";
 import { Menu, Button, Layout, Typography, Icon, Row, Col } from "antd";
 import { Link } from "react-router-dom";
-import styles from "./BaseLayout.module.scss";
 import { LOGOUT } from '../helpers/constant';
 
 const { Header, Content, Footer, Sider } = Layout;
@@ -61,7 +60,6 @@ const SideMenu = () => {
   const menuArray = isLoggedIn ? loggedInMenuArray : loggedOutMenuArray;
   return (
       <Menu
-        className={styles['menu']}
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
         mode="inline"
@@ -71,7 +69,7 @@ const SideMenu = () => {
           return (
             <Menu.Item key={tab.key}>
               <Link to={tab.linkTo}>
-                <Icon type={tab.iconType} filled="true" className={styles['menu-item-icon']} />
+                <Icon type={tab.iconType} filled="true" />
                 <span className="nav-text">{tab.tabName}</span>
               </Link>
             </Menu.Item>
@@ -101,7 +99,6 @@ const BaseLayout = ({ user, children, sider = {}}) => {
   return(
     <Layout>
       <Sider
-        className = {styles['sider']}
         breakpoint="lg"
         width={230}
         collapsedWidth={0}
@@ -110,14 +107,13 @@ const BaseLayout = ({ user, children, sider = {}}) => {
       > 
         <SideMenu />
       </Sider>
-      <Layout className={styles['content-container']}>
+      <Layout >
         <Header style={{paddingLeft: 20}}>
           <Row>
             <Col span = {2}>
               <Icon
                 type={menuToggleState ? "menu-unfold": "menu-fold"}
                 onClick={collapseTrigger}
-                className={styles['menu-fold-unfold-icon']}
               />
             </Col>
             <Col span = {20}>
@@ -128,7 +124,7 @@ const BaseLayout = ({ user, children, sider = {}}) => {
             </Col>
           </Row>
         </Header>
-          <Content className={styles['.content-container']}>
+          <Content>
             {children}
           </Content>
       </Layout>
