4d1db2ec2d94c6cf9684071edef0331544f860dd
adnanraghib1308 <adnan.raghib@gmail.com>
2022-01-18 09:53:42 +0530

    v2 changes

---
+++ build/precache-manifest.fe9990aa6e3900c4cd9d58afa903e3c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2552d0ac3766ceb8e2c0b7d827a8a92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ffed4e0548eebb70c2",
+    "url": "/static/css/2.3c1328bd.chunk.css"
+  },
+  {
+    "revision": "84246498f00d40c02b57",
+    "url": "/static/css/main.db430fef.chunk.css"
+  },
+  {
+    "revision": "39ffed4e0548eebb70c2",
+    "url": "/static/js/2.34182385.chunk.js"
+  },
+  {
+    "revision": "83b6764fc858f218b89e6b80ad292166",
+    "url": "/static/js/2.34182385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84246498f00d40c02b57",
+    "url": "/static/js/main.fe3fbbc7.chunk.js"
+  },
+  {
+    "revision": "fe43d60bbb01d32c51d1",
+    "url": "/static/js/runtime-main.a3a8a1a1.js"
+  }
+]);
